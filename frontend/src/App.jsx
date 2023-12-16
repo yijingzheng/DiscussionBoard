d3b793796c68b52383937bea37f719938ae3402f
yijingzheng <122712551+yijingzheng@users.noreply.github.com>
2023-12-15 19:03:14 -0800

    fix typo in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
-import Login from "./components/login";
+import Login from "./components/Login";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 
